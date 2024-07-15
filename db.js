86ad408cbeb9f1918496e4590a1f543a2295e710
Valentyn Zherevchuk <valikzherevchuk@gmail.com>
2024-07-15 11:04:46 +0300

    setting dot.env

--- db.js
+++ db.js
@@ -1,12 +1,24 @@
 const { Sequelize } = require('sequelize');
 
 module.exports = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
+    process.env.DATABASE_URL,
     {
         dialect: 'postgres',
-        host: process.env.DATABASE_URL,
-        port: process.env.DB_PORT
+        dialectOptions: {
+            ssl: {
+                require: true,
+                rejectUnauthorized: false, // Це потрібно для підключення до бази даних на Heroku
+            },
+        },
     }
 );
+// module.exports = new Sequelize(
+//     process.env.DB_NAME,
+//     process.env.DB_USER,
+//     process.env.DB_PASSWORD,
+//     {
+//         dialect: 'postgres',
+//         host: process.env.DATABASE_URL,
+//         port: process.env.DB_PORT
+//     }
+// );
--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const path = require('path');
 
 const PORT = process.env.PORT || 5000;
 
+
 const app = express();
 app.use(cors());
 app.use(express.json());
@@ -30,3 +31,6 @@ const start = async () => {
 }
 
 start();
+
+
+console.log(__dirname);
